8e117ec1f7db8733bcf0b52e40af28a7c2c2e895
Cristopher-91 <k.piasecki91@gmail.com>
2021-01-29 20:08:05 +0100

    Added menu for desktop view - need few adjustments

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,25 @@ window.onload = function(){
     document.getElementById("current-year").innerHTML = current_year;
 }
 
+//Navbar change on scroll
+$(document).ready(function () {
+
+    window.onscroll = function () { navbarChange() };
+
+    let navbar = document.getElementById("nav-main");
+    let navLogo = document.getElementById("nav-logo");
+
+    let change = navbar.offsetTop;
+
+    function navbarChange() {
+        if (window.pageYOffset > change) {
+            navbar.classList.add("navbar-white");
+            navLogo.classList.remove("navbar-brand-logo-white");
+
+        } else {
+            navbar.classList.remove("navbar-white");
+            navLogo.classList.add("navbar-brand-logo-white");
+        }
+    }
+
+});
