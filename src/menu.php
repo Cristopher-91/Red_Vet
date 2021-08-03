ce3524d3903a7f2fe9be73c1c4dfee6918dfb585
Cristopher-91 <k.piasecki91@gmail.com>
2021-08-03 10:53:10 +0200

    put menu on php

---
+++ src/menu.php
@@ -0,0 +1,45 @@
+<nav class="navbar navbar-dark fixed-top navbar-expand-lg" id="nav-main">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="img/Red-Vet-Logo-Flatt.png" alt="Logo Przychodni Red-Vet"
+            class="navbar-brand-logo navbar-brand-logo-white" id="nav-logo" /></a>
+        <!--<button type="button" class="navbar-toggler custom-toggler" data-toggle="collapse" data-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        -->
+        <div id="nav-hamburger" class="d-lg-none navbar-hamburger-white">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="about-dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">O nas</a>
+            <div class="dropdown-menu" aria-labelledby="about-dropdown">
+              <a class="dropdown-item" href="/personel">Personel</a>
+              <a class="dropdown-item" href="/przychodnia">Przychodnia</a>
+            </div>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="services-dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Usługi</a>
+            <div class="dropdown-menu" aria-labelledby="about-dropdown">
+              <a class="dropdown-item" href="/szczepienia">Szczepienia</a>
+              <a class="dropdown-item" href="/kastracja">Kastracja</a>
+              <a class="dropdown-item" href="/chirurgia">Chirurgia</a>
+              <a class="dropdown-item" href="/wizyty-domowe">Wizyty domowe</a>
+              <a class="dropdown-item" href="/paszporty-zwierzece">Paszporty zwierzęce</a>
+              <a class="dropdown-item" href="/czipowanie">Czipowanie</a>
+              <a class="dropdown-item" href="/profilaktyka">Profilaktyka</a>
+              <a class="dropdown-item" href="/badania-laboratoryjne">Badania laboratoryjne</a>
+            </div>
+          </li>
+          <li class="nav-item"><a class="nav-link" href="#sklep">Sklep</a></li>
+          <li class="nav-item"><a class="nav-link" href="#aktualnosci">Aktualności</a></li>
+          <li class="nav-item"><a class="nav-link" href="#opinie">Opinie</a></li>
+          <li class="nav-item"><a class="nav-link" href="#kontakt">Kontakt</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
